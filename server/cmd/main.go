57a73fc2cd8cfbffad3f26e1942c52d39bfbee40
abraao <abraao-284@outlook.com>
2025-11-05 21:02:09 -0300

    fix: removing the placehold data and setting the env in the database string connect and adding the basic main to test

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"server/db"
+)
 
-func main (){
-	
+func main() {
+	dbConn, err := db.NewDatabase()
+	if err != nil {
+		log.Fatalf("could not initialize database connection: %s", err)
+	}
+
+	defer dbConn.CloseDatabase()
+	http.ListenAndServe(":8080", nil)
+
 }
--- server/db/db.go
+++ server/db/db.go
@@ -1,14 +1,22 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"server/utils"
+
+	_ "github.com/lib/pq"
+)
 
 type Database struct {
 	db *sql.DB
 }
 
+func NewDatabase() (*Database, error) {
+	env := utils.GetEnv()
+	connectString := fmt.Sprintf("postgresql://%s@%s@%s:%s/%s", env.Db_User, env.Db_Pass, env.Db_Host, env.Db_Port, env.Db_Table)
 
-func NewDatabase() (*Database, error){
-	db, err := sql.Open("postgres", "postgresql://admin@admin@postgres:5432/tarefas")
+	db, err := sql.Open("postgres", connectString)
 
 	if err != nil {
 		return nil, err
@@ -17,10 +25,10 @@ func NewDatabase() (*Database, error){
 	return &Database{db: db}, nil
 }
 
-func (d *Database) CloseDatabase(){
+func (d *Database) CloseDatabase() {
 	d.db.Close()
 }
 
-func (d * Database) GetDB() *sql.DB{
+func (d *Database) GetDB() *sql.DB {
 	return d.db
 }
